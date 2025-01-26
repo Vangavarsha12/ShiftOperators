ff2f076423d0c81d63bc8ad6e30d50d42bf1e674
Vangavarsha12 <varshavanga38@gmail.com>
2025-01-26 18:38:01 +0530

    Add files via upload

---
+++ Shiftoperator.java
@@ -0,0 +1,11 @@
+public class Shiftoperator {
+    public static void main(String[] args) {
+        int a=10;
+        int leftshift=a<<2;
+        System.out.println("left shift of "+a+" is "+leftshift);
+        int rightshift=a>>1;
+        System.out.println("right shift of "+a+" is "+rightshift);
+
+    }
+    
+}
